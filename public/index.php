a5250b2c562bc1b0e5274e7844f75a672edb691b
fortheday <fortheday@126.com>
2018-08-29 09:04:49 +0800

    添加view函数

--- public/index.php
+++ public/index.php
@@ -11,17 +11,15 @@ function autoload($class)
 }
 spl_autoload_register('autoload');
 
-
-
 $userController = new controllers\UserController;
 $userController->hello();
-
 
 // 加载视图
 // 参数一、加载的视图的文件名
 // 参数二、向视图中传的数据
 function view($viewFileName, $data = [])
 {
+    // 解压数组成变量
     extract($data);
 
     $path = str_replace('.', '/', $viewFileName) . '.html';
