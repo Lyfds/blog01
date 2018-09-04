67eb4771a9764f0ae187153aa4cc6dbf77f07ac4
fortheday <fortheday@126.com>
2018-09-04 10:02:15 +0800

    添加日志类

--- libs/Log.php
+++ libs/Log.php
@@ -14,6 +14,12 @@ class Log
     // 向日志文件中追加内容
     public function log($content)
     {
-        fwrite($this->fp, $content . "\r\n");
+        // 获取当前时间
+        $date = date('Y-m-d H:i:s');
+        // 拼出日志内容的格式  （在文件中 "\r\n" 是换行的意思）
+        $c = $date . "\r\n";
+        $c .= str_repeat('=', 120) . "\r\n";    //  str_repeat：获取 30 个 =
+        $c .= $content . "\r\n\r\n";
+        fwrite($this->fp, $c);
     }
 }
