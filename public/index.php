8042705ea75fa7383b45e2f36f5efce232263291
fortheday <fortheday@126.com>
2018-09-07 11:58:07 +0800

    集成支付宝支付

--- public/index.php
+++ public/index.php
@@ -6,15 +6,15 @@ ini_set('session.save_path', 'tcp://127.0.0.1:32768?database=3');
 
 session_start();
 
-// 如果用户以 POST 方式访问网站时，需要验证令牌
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-    if(!isset($_POST['_token']))
-        die('违法操作！');
+// 如果用户以 POST 方式访问网站时，需要验证令牌(防止外站提交)
+// if($_SERVER['REQUEST_METHOD'] == 'POST')
+// {
+//     if(!isset($_POST['_token']))
+//         die('违法操作！');
 
-    if($_POST['_token'] != $_SESSION['token'])
-        die('违法操作！');
-}
+//     if($_POST['_token'] != $_SESSION['token'])
+//         die('违法操作！');
+// }
 
 // 定义常量
 define('ROOT', dirname(__FILE__) . '/../');
